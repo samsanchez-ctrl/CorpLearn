4bfb947fbce4c6c96f7734dc9ef1d444ec39c2f3
DilanFuentes <fdilan069@gmail.com>
2026-04-21 08:55:59 -0400

    SpringBoot

---
+++ learn/src/test/java/com/corp/learn/LearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corp.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
