dfb156171d667d017356502df1496bcb008ae03d
samsanchez-ctrl <sam.sanchez@duocuc.cl>
2026-04-21 17:05:52 -0400

    UsuarioController se configurar para los endpoints de registro e inicio de sesion

--- learn/src/main/java/com/corp/learn/controller/UsuarioController.java
+++ learn/src/main/java/com/corp/learn/controller/UsuarioController.java
@@ -1,5 +1,35 @@
 package com.corp.learn.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.corp.learn.model.Usuario;
+import com.corp.learn.service.UsuarioService;
+
+@RestController
+@RequestMapping("/usuarios")
+@CrossOrigin(origins = "http://localhost:4200") // Para conectar con Angular
 public class UsuarioController {
     
+    private final UsuarioService usuarioService;
+
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<Usuario> registrar(@RequestBody Usuario usuario) {
+        return ResponseEntity.ok(usuarioService.registrarUsuario(usuario));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody Usuario loginRequest) {
+        return usuarioService.login(loginRequest.getEmail(), loginRequest.getPassword())
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.status(401).build());
+    }
 }
