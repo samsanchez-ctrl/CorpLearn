343c5169ccc44b77393dc4063f6b856434bfa923
samsanchez-ctrl <sam.sanchez@duocuc.cl>
2026-04-27 08:26:50 -0400

    actualizo usuario

--- usuario/src/main/java/com/micriservicio/usuario/controller/UsuarioController.java
+++ usuario/src/main/java/com/micriservicio/usuario/controller/UsuarioController.java
@@ -1,16 +1,14 @@
 package com.micriservicio.usuario.controller;
 
+import com.micriservicio.usuario.model.Usuario;
+import com.micriservicio.usuario.service.UsuarioService;
 import java.util.List;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.micriservicio.usuario.model.Usuario;
-import com.micriservicio.usuario.service.UsuarioService;
 
 @RestController
 @RequestMapping("/usuarios")
@@ -34,7 +32,7 @@ public class UsuarioController {
                 .orElse(ResponseEntity.status(401).build());
     }
 
-    @GetMapping("/listaUsuarios")
+    @GetMapping("")
     public ResponseEntity<List<Usuario>> listarUsuarios() {
         List<Usuario> usuarios = usuarioService.obtenerTodosLosUsuarios();
         return ResponseEntity.ok(usuarios);
