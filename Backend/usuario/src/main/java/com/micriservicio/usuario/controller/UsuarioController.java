3313c6ad0800883e715df3b8cc48f18846bf6991
samsanchez-ctrl <sam.sanchez@duocuc.cl>
2026-04-30 10:45:15 -0400

    se modifico usuario

--- Backend/usuario/src/main/java/com/micriservicio/usuario/controller/UsuarioController.java
+++ Backend/usuario/src/main/java/com/micriservicio/usuario/controller/UsuarioController.java
@@ -3,6 +3,8 @@ package com.micriservicio.usuario.controller;
 import com.micriservicio.usuario.model.Usuario;
 import com.micriservicio.usuario.service.UsuarioService;
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,28 +15,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/usuarios")
 public class UsuarioController {
-    
-    private final UsuarioService usuarioService;
-
-    public UsuarioController(UsuarioService usuarioService) {
-        this.usuarioService = usuarioService;
-    }
+    @Autowired
+    private UsuarioService service;
 
     @PostMapping("/register")
     public ResponseEntity<Usuario> registrar(@RequestBody Usuario usuario) {
-        return ResponseEntity.ok(usuarioService.registrarUsuario(usuario));
+        return ResponseEntity.ok(service.registrarUsuario(usuario));
     }
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody Usuario loginRequest) {
-        return usuarioService.login(loginRequest.getEmail(), loginRequest.getPassword())
+        return service.login(loginRequest.getEmail(), loginRequest.getPassword())
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.status(401).build());
     }
 
     @GetMapping("")
     public ResponseEntity<List<Usuario>> listarUsuarios() {
-        List<Usuario> usuarios = usuarioService.obtenerTodosLosUsuarios();
+        List<Usuario> usuarios = service.obtenerTodosLosUsuarios();
         return ResponseEntity.ok(usuarios);
     }
 }
