140d39b1f8a64da7327894c13741da3c546ba117
samsanchez-ctrl <sam.sanchez@duocuc.cl>
2026-04-30 10:30:08 -0400

    se modifico modulo

--- Backend/modulo/src/main/java/com/microservicio/modulo/model/Modulo.java
+++ Backend/modulo/src/main/java/com/microservicio/modulo/model/Modulo.java
@@ -5,8 +5,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
 
 @Entity
+@Table(name = "MODULO")
+@Data
 public class Modulo {
     
     @Id
@@ -15,24 +19,4 @@ public class Modulo {
 
     private String nombre; 
     private String descripcion;
-
-
-    public Modulo() {}
-
-    public Modulo(Long id, String nombre, String descripcion){
-        this.id = id;
-        this.nombre = nombre;
-        this.descripcion = descripcion;
-    }
-
-    public Long getId(){ return id; }
-    public void setId(Long id) { this.id = id; }
-    
-    public String getNombre() { return nombre; }
-    public void setNombre(String nombre) {this.nombre = nombre; }
-    
-    public String getDescripcion() { return descripcion; }
-    public void setDescripcion(String descripcion) {this.descripcion = descripcion;}
-
-
 }
