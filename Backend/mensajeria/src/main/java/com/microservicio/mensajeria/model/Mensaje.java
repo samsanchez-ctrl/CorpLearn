5758415d8a5e500722b4ed7812445fea68881b59
samsanchez-ctrl <sam.sanchez@duocuc.cl>
2026-04-30 10:25:52 -0400

    se modifico mensajeria

--- Backend/mensajeria/src/main/java/com/microservicio/mensajeria/model/Mensaje.java
+++ Backend/mensajeria/src/main/java/com/microservicio/mensajeria/model/Mensaje.java
@@ -6,8 +6,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
 
 @Entity
+@Table(name = "MENSAJERIA")
+@Data
 public class Mensaje {
 
     @Id
@@ -19,29 +23,4 @@ public class Mensaje {
     private String contenido;
 
     private LocalDateTime fecha;
-
-    public Mensaje() {}
-
-    public Mensaje(Long id, String emisor, String receptor, String contenido, LocalDateTime fecha){
-        this.id = id;
-        this.emisor = emisor;
-        this.receptor = receptor;
-        this.contenido = contenido;
-        this.fecha = fecha;
-    }
-
-    public Long getId() { return id; }
-
-    public String getEmisor() { return emisor; }
-    public void setEmisor(String emisor) { this.emisor = emisor; }
-
-    public String getReceptor() { return receptor; }
-    public void setReceptor(String receptor) { this.receptor = receptor; }
-
-    public String getContenido() { return contenido; }
-    public void setContenido(String contenido) { this.contenido = contenido; }
-
-    public LocalDateTime getFecha() { return fecha; }
-    public void setFecha(LocalDateTime fecha) { this.fecha = fecha; }
-    
 }
