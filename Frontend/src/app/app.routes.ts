1e77c2434a71fac5e7f2ee17e8889e9ea38ef1db
samsanchez-ctrl <sam.sanchez@duocuc.cl>
2026-05-13 09:52:23 -0400

    actualizando front

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login-component/login-component';
+import { ListUsuario } from './pages/list-usuario/list-usuario';
 
 export const routes: Routes = [
     {
         path: 'login',
         component: LoginComponent,
     },
+    {
+        path: 'list',
+        component: ListUsuario,
+    }
 ];
